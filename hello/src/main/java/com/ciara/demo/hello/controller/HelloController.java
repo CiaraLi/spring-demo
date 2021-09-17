ec296634bd7ab9e7d8462af5f56a98df4bf1f6f4
liwei24 <liwei24@100tal.com>
2021-09-17 18:16:08 +0800

    demo

--- hello/src/main/java/com/ciara/demo/hello/controller/HelloController.java
+++ hello/src/main/java/com/ciara/demo/hello/controller/HelloController.java
@@ -10,16 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    //直接注入环境变量
-    @Autowired
-    private Environment env;
-    //读取配置
+    //读取配置 1.1、读取配置
     @Value("${site.title}")
     private String title;
-    //读物数组配置
+    //读取配置 1.2读物数组配置
     @Value("${allows[0]}")
     private String allows;
-    // 注入配置对象
+    //读取配置 2、直接注入环境变量
+    @Autowired
+    private Environment env;
+    //读取配置 3、注入配置对象
     @Autowired
     private Site site;
 
@@ -30,6 +30,7 @@ public class HelloController {
         return "Hello Spring:" + title;
     }
 
+    //https://www.bilibili.com/video/BV1Lq4y1J77x?p=12&spm_id_from=pageDriver 教学视频
     @RequestMapping("/site")
     public String site() {
         return "欢迎来到" + site.getAuthor() + "小站：" + site.getTitle();
